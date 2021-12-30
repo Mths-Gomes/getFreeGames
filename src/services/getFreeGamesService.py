a3678eb744fb940a7fcfa6d79ca080ab2964ac1d
Matheus Gomes <mth.gom@gmail.com>
2021-12-30 16:50:12 -0300

    Fix previous commit

--- src/services/getFreeGamesService.py
+++ src/services/getFreeGamesService.py
@@ -1,12 +1,26 @@
 import pyautogui as rb
 import time
 
+from services.countFreeGamesService import countFreeGames
+from services.unableToGetFreeGamesService import unableToGetFreeGames
 from services.sendConfirmationPrintService import sendConfirmationPrint
+
 
 rb.PAUSE = 1
 
 link = 'https://www.epicgames.com/store/pt-BR/free-games'
 # link = 'https://www.epicgames.com/store/pt-BR/'
+
+def getFreeGames():
+  quantity = countFreeGames()
+  if(quantity == 3):
+    getThreeFreeGames()
+  elif(quantity == 2):
+    getTwoFreeGames()
+  elif(quantity ==1):
+    getOneFreeGames()
+  else:
+    unableToGetFreeGames()
 
 def getOneFreeGames():
   # Abre o chrome
@@ -46,6 +60,42 @@ def getOneFreeGames():
   sendConfirmationPrint()
 
 def getTwoFreeGames():
+  rb.press('winleft')
+  rb.write('chrome')
+  rb.press('enter')
+
+  #Seleciona user browser
+  # time.sleep(3)
+  # rb.click(1017,570)
+
+  #Nova guia e direcionamento Epic Store
+  rb.hotkey('ctrl', 't')
+  rb.write(link)
+  rb.press('enter')
+
+  #Clica no banner de game gratis
+  time.sleep(7)
+  rb.click(386, 523)
+
+  # Pop-up +18
+  time.sleep(5)
+  rb.click(688, 469)
+
+  #Clica em obter
+  time.sleep(6)
+  rb.click(1072, 557)
+
+  # #Confirma a "compra"
+  time.sleep(6)
+  rb.click(1014, 692)
+
+  #Fecha card de agradecimento
+  time.sleep(6)
+  rb.click(945, 118)
+
+  sendConfirmationPrint()
+
+def getThreeFreeGames():
   rb.press('winleft')
   rb.write('chrome')
   rb.press('enter')
